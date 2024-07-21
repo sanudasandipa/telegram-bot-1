8f3ecc66d7bf7cb681b8eb9531de91efe218f79c
Sanuda Sandipa <sanudasandipa29@gmail.com>
2024-07-21 12:20:04 +0530

    first

---
+++ sabnzbdapi/__init__.py
@@ -0,0 +1,5 @@
+from sabnzbdapi.requests import sabnzbdClient
+
+__all__ = [
+    "sabnzbdClient"
+]
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run the update script
+python3 update.py
+
+# Start the bot
+python3 -m bot
